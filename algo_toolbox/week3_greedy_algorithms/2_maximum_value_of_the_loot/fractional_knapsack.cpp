f85a26a79b4d3f7bf14e23c23ad427d2f8519a33
mayankkanela <mayankkanela8@gmail.com>
2020-02-18 00:16:17 +0530

    knapsack

--- algo_toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ algo_toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,20 +1,77 @@
+// Task. The goal of this code problem is to implement an algorithm for the fractional knapsack problem.
+
+// Input Format. The first line of the input contains the number 𝑛 of items and the capacity 𝑊 of a knapsack.
+// The next 𝑛 lines define the values and weights of the items. The 𝑖-th line contains integers 𝑣𝑖 and 𝑤𝑖—the
+// value and the weight of 𝑖-th item, respectively.
+
+// Constraints. 1 ≤ 𝑛 ≤ 103, 0 ≤ 𝑊 ≤ 2 · 106; 0 ≤ 𝑣𝑖 ≤ 2 · 106, 0 < 𝑤𝑖 ≤ 2 · 106 for all 1 ≤ 𝑖 ≤ 𝑛. All the
+// numbers are integers.
+
+// Output Format. Output the maximal value of fractions of items that fit into the knapsack. The absolute
+// value of the difference between the answer of your program and the optimal value should be at most
+// 10−3. To ensure this, output your answer with at least four digits after the decimal point (otherwise
+// your answer, while being computed correctly, can turn out to be wrong because of rounding issues).
+
+// Sample 1.
+// Input:
+// 3 50
+// 60 20
+// 100 50
+// 120 30
+// Output:
+// 180.0000
+// To achieve the value 180, we take the first item and the third item into the bag.
 #include <iostream>
 #include <vector>
 
+using namespace std;
 using std::vector;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
-  double value = 0.0;
-
+double get_optimal_value(double capacity, vector<int> weights, vector<int> values) {
+  double value = 0.0;         
   // write your code here
+    unsigned int k = 0;
+    while(k < weights.size()){
+      if(capacity == 0)
+        return value; 
+      double tempValue = values[0];
+      double tempWeight = weights[0]; 
+      int c  = 0;
+      for(int i = 1; i < values.size(); i++){
+        if(tempWeight == 0)
+          tempWeight = INT8_MIN;
+        if(weights[i] > 0)  
+        if(tempValue / tempWeight < values[i] / weights[i]){
+          tempValue = values[i];    
+          tempWeight = weights[i];
+          c = i;
+        }
+      
+      }
+      
+      values[c] = INT8_MIN;
+      weights[c] = INT8_MIN;
+
+      if(tempWeight > 0)
+      if(capacity > tempWeight){
+        value += tempValue;
+        capacity = capacity - tempWeight;
+      }
+      else
+      {
+        value += (double) capacity * (tempValue / tempWeight);
+        capacity = 0;
+      }
+      k++;
+    }
 
   return value;
 }
 
 int main() {
   int n;
-  int capacity;
-  std::cin >> n >> capacity;
+  double capacity;
+  cin >> n >> capacity;
   vector<int> values(n);
   vector<int> weights(n);
   for (int i = 0; i < n; i++) {
@@ -22,8 +79,7 @@ int main() {
   }
 
   double optimal_value = get_optimal_value(capacity, weights, values);
-
-  std::cout.precision(10);
+  cout << fixed;
+  std::cout.precision(3) ;
   std::cout << optimal_value << std::endl;
-  return 0;
 }
