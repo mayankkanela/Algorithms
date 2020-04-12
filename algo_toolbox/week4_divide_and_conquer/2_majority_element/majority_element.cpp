be59815d5d22d615d03fef38fc84e9cc8fb132ef
mayankkanela <mayankkanela8@gmail.com>
2020-04-12 12:18:42 +0530

    majority element

--- algo_toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ algo_toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -8,6 +8,27 @@ int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
   //write your code here
+  int leftElement = get_majority_element(a, left, (left + right - 1) / 2 + 1);
+  int rightElement = get_majority_element(a, (left + right - 1) / 2 + 1, right);
+
+  int lc = 0;
+  for (int i = left; i < right; i++)
+  {
+    if(a[i] == leftElement)
+      lc++;
+  }
+  if(lc > (right  - left) / 2)
+  return leftElement;
+
+  int rc = 0;
+  for (int i = left; i < right; i++)
+  {
+    if(a[i] == rightElement)
+      lc++;
+  }
+  if(lc > (right  - left) / 2)
+  return rightElement;
+  
   return -1;
 }
 
